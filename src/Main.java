03886e104a55c70ab9da9be0aba2718d12b8d2e5
Westender <javaxwestender@gmail.com>
2023-07-17 20:09:08 +0300

    Add: task1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Main {
+
+    public static void main(String[] args) {
+        task1();
+    }
+
+    static void task1(){
+        System.out.println("Задание №1:");
+        System.out.println("Введите возраст человека");
+
+        Scanner sc = new Scanner(System.in);
+
+        int age = sc.nextInt();
+        sc.close();
+
+        if (age<0){
+            System.out.println("Этот человек еще не родился");
+        } else if (age > 0 && age < 18){
+            System.out.println("Если возраст человека равен " + age + ", то он не достиг совершеннолетия, нужно немного подождать");
+        } else if (age >=18 && age < 118){
+            System.out.println("Если возраст человека равен " + age + ", то он совершеннолетний");
+        } else {
+            System.out.println("Так долго не живут =)");
+        }
     }
 }
